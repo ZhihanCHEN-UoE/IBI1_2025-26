7f3b26befc828774a87a70edb85eb602532213d2
Zhihan CHEN <albedochen@outlook.com>
2026-03-11 13:18:00 +0800

    Refactor population growth calculations
    Refactor population variables and comments for clarity.

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,9 +1,9 @@
-a = 5080000
-b = 5330000
-c = 5550000
-d = b-a
-e = c-b
-if d>e:
+a = 5080000 #population in 2004
+b = 5330000 #population in 2014
+c = 5550000 #population in 2024
+d = b-a #calculate the growth between 2004 and 2014"
+e = c-b #calculate the growth between 2014 and 2024"
+if d>e: #compare
     print("d is larger, that means population growth decelerating")
 elif d == e:
     print("they are equal")
