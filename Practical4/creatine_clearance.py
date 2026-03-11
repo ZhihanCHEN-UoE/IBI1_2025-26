04a46a28a920376ea507237e316bb79dce45152c
Zhihan CHEN <albedochen@outlook.com>
2026-03-11 13:11:47 +0800

    Fix variable name typo from 'genger' to 'gender'

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -6,7 +6,7 @@ weigh = int(input("weight (in kg)"))
 while weigh <= 20 or weigh >= 80:
     print("please input a valid weigh!")
     weigh = int(input("weight (in kg)"))
-genger = input("gender, for male please input 0, for female please input 1")
+gender = input("gender, for male please input 0, for female please input 1")
 gender = [1,0.85][gender]
 cr = input("Cr")
 rate = ((140-age)*weigh)/(72*cr)*gender
