0f0c0b6d90487168ab2371337d1237a228a4adda
Zhihan CHEN <albedochen@outlook.com>
2026-03-11 13:14:41 +0800

    Fix formatting issue in comparison condition

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -5,7 +5,7 @@ d = b-a
 e = c-b
 if d>e:
     print("d is larger, that means population growth decelerating")
-elif d ==e:
+elif d == e:
     print("they are equal")
 else:
     print("e is larger,that means population growth accelerating")
